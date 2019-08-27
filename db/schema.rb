d60b061d044abae825a9befe34224e1045c20de8
root <root@sd-147381.dedibox.fr>
2019-08-27 17:06:58 +0200

    [wip] replace sqlite3 with pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2019_08_27_143319) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stories", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.json "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -36,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_08_27_143319) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "stories", "users"
 end
