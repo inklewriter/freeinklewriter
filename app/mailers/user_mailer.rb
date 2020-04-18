fb4ac7f5b6d24c50cda960c553691d26a7041ebe
Maxence Malbois <maxencemalbois@gmail.com>
2020-04-18 12:31:06 +0200

    added strict email regexp before sending email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,10 @@ class UserMailer < ApplicationMailer
 
 	def test(id)
 		@user = User.find_by(email: id)
-		mail(to: @user.email, subject: "email de test")
+		# testing if USER email field is a real email address. Regexp taken here http://emailregex.com/
+		# then email is sent
+		if @user.match /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+			mail(to: @user.email, subject: "email de test")
+		end
 	end
 end
