fe86e7dd4c9b72fa7018f8459c0f06ec39d179e8
Maxence Malbois <maxencemalbois@gmail.com>
2020-04-19 12:14:58 +0200

    regexp fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
 		@user = User.find_by(email: id)
 		# testing if USER email field is a real email address. Regexp taken here http://emailregex.com/
 		# then email is sent
-		if @user.match /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+		if @user.email.match /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 			mail(to: @user.email, subject: "email de test")
 		end
 	end
