f9d5fcdb872498e099883072c07f98f2e306cf0b
Maxence Malbois <maxencemalbois@gmail.com>
2020-04-24 15:06:35 +0200

    fix password renewal

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
