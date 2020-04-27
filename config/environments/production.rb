7abd8b5c5d8fbfc8607fc0825f1a30e7c8b9b0cd
alban <git@albancrommer.com>
2020-04-27 11:52:11 +0200

    [fix] the prod environment should have full mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.default_url_options = { host: ENV["ACTION_MAILER_HOST"],  :protocol => 'https'}
+
   config.action_mailer.smtp_settings = {
       address: ENV["MAILING_ADDRESS"],
       port: ENV["MAILING_PORT"],
