ea18af8605d2613d31bc9142c4f597305873155b
Maxence Malbois <maxencemalbois@gmail.com>
2020-05-25 16:37:32 +0200

    [feat] health check page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,15 @@ class PagesController < ApplicationController
 
 	def index
 	end
+
+	def health
+		@status = {}
+		@status[:database_connected] = ::ActiveRecord::Base.connection_pool.with_connection(&:active?) rescue false
+
+		respond_to do |format|
+				format.html 
+				format.json {render json: @status}				
+		end
+
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
 
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
+  get 'health', to: 'pages#health'
 
 end
