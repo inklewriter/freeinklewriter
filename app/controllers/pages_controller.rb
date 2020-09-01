92358951ab03ea5c86d0851c04049f8d94bb9c89
alban <git@albancrommer.com>
2020-09-01 10:24:15 +0200

    [wip] there should be a route for privacy page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,18 @@ class PagesController < ApplicationController
 	def index
 	end
 
+    def privacy 
+		respond_to do |format|
+				format.html 
+		end
+    end
 	def health
 		@status = {}
 		@status[:database_connected] = ::ActiveRecord::Base.connection_pool.with_connection(&:active?) rescue false
 
 		respond_to do |format|
 				format.html 
-				format.json {render json: @status}				
+				format.json {render json: @status}
 		end
 
 	end
