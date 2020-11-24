108ab594039ba40efe85b52e1dc4e537604bd11e
Maxence Malbois <maxencemalbois@gmail.com>
2020-11-24 13:05:01 +0100

    [enh] isolating inkle css and basic stories stats for admins

--- app/controllers/admin/adminpages_controller.rb
+++ app/controllers/admin/adminpages_controller.rb
@@ -11,5 +11,23 @@ class Admin::AdminpagesController < Admin::AuthorizerController
 	# specific logic is handled by specific Admin namespaced controllers in controllers/admin folder
 
 	def index
+		
+		@current_year = Time.now.year.to_i
+		@last_three_years = {@current_year.to_s => stories_created_monthly_in(@current_year), (@current_year-1).to_s => stories_created_monthly_in(@current_year-1), (@current_year-2).to_s => stories_created_monthly_in(@current_year-2) }
+
+	end
+
+
+
+	private
+
+	def stories_created_monthly_in(year)
+		monthly_stories = {}
+		if year.to_i.in?(2000..2050)
+			12.times do |m|
+				monthly_stories[(m+1).to_s] = Story.where(created_at: Date.civil(year, m+1, 1)..Date.civil(year, m+1, -1)).count
+			end
+		end	
+		return monthly_stories
 	end
 end
