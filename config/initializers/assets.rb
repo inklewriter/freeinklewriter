d22908d0bfbce8aecbafff7b450f7b28fb75fe05
Maxence Malbois <maxencemalbois@gmail.com>
2021-03-20 23:32:28 +0100

    [fix] added all common assets to assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
 
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg *.ttf *.webp *.woff *.woff2 *.eot)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
