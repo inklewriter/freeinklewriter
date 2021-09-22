ae6147cf258797fc5b8b859508f4a485a2423e96
alban <git@albancrommer.com>
2021-09-22 14:34:59 +0200

    Revert "[enh] Airbrake for prod only"
    This reverts commit 5905e3627659dd1c13e4ef1a7f2f0fa3b86b6bb8.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -35,7 +35,7 @@ Airbrake.configure do |c|
   # NOTE: This option must be set in order to make the 'ignore_environments'
   # option work.
   # https://github.com/airbrake/airbrake-ruby#environment
-  c.environment = :production
+  c.environment = Rails.env
 
   # Setting this option allows Airbrake to filter exceptions occurring in
   # unwanted environments such as :test.
