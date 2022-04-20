c01c81483877d72e641ef905fde9b9a1d04e2e10
Maxence Malbois <maxencemalbois@gmail.com>
2022-04-20 10:05:54 +0200

    [enh] configured exporter pack to compress CSS, JS and SVG files into Brotli

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,10 @@ module Freeifwriter
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.assets.configure do |env|
+      env.register_exporter %w(text/css application/javascript image/svg+xml), Sprockets::ExportersPack::BrotliExporter
+    end
+    
   end
 end
